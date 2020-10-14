26fb5e1fcbb6c472cf7dff4ae4ba5d7b3e963fe1
Karin Hsu <khsu0720@gmail.com>
2020-10-14 10:15:51 +0800

    "Feature" "Add action items section"

---
+++ src/ActionItems.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import StartBtn from './StartBtn';
+
+const ActionItems = () => {
+  return  (
+    <div className="actionItems">
+      Action items
+      <StartBtn />
+    </div>
+  )
+}
+
+export default ActionItems;
