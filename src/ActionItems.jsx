b0bfa10b27d083c7bcd72b5ea62325e267616df1
Karin Hsu <khsu0720@gmail.com>
2020-10-15 18:39:40 +0800

    "Style" "Add action item icons"

--- src/ActionItems.jsx
+++ src/ActionItems.jsx
@@ -1,11 +1,20 @@
-import React, { useState } from 'react';
-import StartBtn from './StartBtn';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUndo } from '@fortawesome/free-solid-svg-icons'
 
-const ActionItems = () => {
+const ActionItems = ({active, resetActive, yPoint, handleClick, resetCount, deleteGraph}) => {
+  
+  let displayVal = yPoint;
+  if (active) {
+    displayVal = 0;
+  } else {
+    displayVal = 1;
+  }
   return  (
     <div className="actionItems">
-      Action items
-      <StartBtn />
+      { active && <FontAwesomeIcon className={`btn icon ${resetActive && "resetActive"}`} onClick={deleteGraph} icon={faUndo} size="xs" />}
+      <button type="button" className={`btn btn--primary ${active && "active--primary"}`} onClick={handleClick}>{active ? yPoint === 0 ? "01" : yPoint > 9 ? yPoint : "0" + yPoint : "Start"}</button>
+      <button type="button" className={`${active ? "active" : 'inactive'}`} onClick={resetCount}>Reset</button>
     </div>
   )
 }
