a034a27d143c5b077dbddd568615f7e5fa9ea025
Karin Hsu <khsu0720@gmail.com>
2020-10-15 18:40:48 +0800

    "add chart container"

--- src/ChartContainer.jsx
+++ src/ChartContainer.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import Graph from './Graph';
 import ActionItems from './ActionItems';
 
-const ChartContainer = ({data, handleClick}) => {
+const ChartContainer = ({data, active, resetActive, xPoint, yPoint, handleClick, resetCount, deleteGraph}) => {
   return (
     <div className="chartContainer">
-      Accounter
-      <Graph data={data} handleClick={handleClick} />
-      <ActionItems />
+      <p>Accounter</p>
+      <Graph data={data} xPoint={xPoint} />
+      <ActionItems handleClick={handleClick} resetActive={resetActive} yPoint={yPoint} active={active} resetCount={resetCount} deleteGraph={deleteGraph} />
     </div>
   )
 };
